8f3cf5bc305d0a7050da40a4baa152c3e984d07f
shimanamisan <mikan.sup.all@gmail.com>
2021-07-20 18:13:29 +0900

    [update] watch前にビルドする処理を追加

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
-const config = require('./gulp/config')
+const config = require('./gulp/config');
 const $ = require('./gulp/pulugin');
 const { ejsTask } = require('./gulp/tasks/ejsBuild');
-const { sassTask} = require('./gulp/tasks/sassBuild');
+const { sassTask } = require('./gulp/tasks/sassBuild');
 const { imgTask } = require('./gulp/tasks/imgBuild');
 const { jsTask } = require('./gulp/tasks/jsBuild');
 const { serverTask } = require('./gulp/tasks/localServer');
@@ -10,7 +10,6 @@ const { serverTask } = require('./gulp/tasks/localServer');
 // watch
 //============================================================
 const watch = function (done) {
-
     // 監視するタスク
     $.gulp.watch([config.ejs.src, config.ejs.meta], $.gulp.series(ejsTask));
     $.gulp.watch(config.ejs.src, $.gulp.series(ejsTask));
@@ -19,11 +18,24 @@ const watch = function (done) {
     $.gulp.watch(config.js.src, $.gulp.series(jsTask));
 
     // 変更があったらリロードする
-    $.gulp.watch(config.ejs.dist).on('change', $.browserSync.reload)
-    $.gulp.watch(config.sass.dist).on('change', $.browserSync.reload)
-    $.gulp.watch(config.js.dist).on('change', $.browserSync.reload)
+    $.gulp.watch(config.ejs.dist).on('change', $.browserSync.reload);
+    $.gulp.watch(config.sass.dist).on('change', $.browserSync.reload);
+    $.gulp.watch(config.js.dist).on('change', $.browserSync.reload);
 
     done();
-}
+};
 
-exports.default = $.gulp.series(watch, serverTask);
+//============================================================
+// build
+//============================================================
+const build = function (done) {
+    
+    ejsTask(done);
+    sassTask(done);
+    imgTask(done);
+    jsTask(done);
+
+    done();
+};
+
+exports.default = $.gulp.parallel(build, watch, serverTask);
